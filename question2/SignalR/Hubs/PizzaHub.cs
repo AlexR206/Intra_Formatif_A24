7d65d4dfb8d8e8b92420f1648ef4b0c4fdb8620c
Alexandre Remy <2061660@cegepmontpetit.ca>
2025-10-06 00:05:05 -0400

    FCT - SignalR

--- question2/SignalR/Hubs/PizzaHub.cs
+++ question2/SignalR/Hubs/PizzaHub.cs
@@ -13,28 +13,67 @@ namespace SignalR.Hubs
 
         public override async Task OnConnectedAsync()
         {
+            _pizzaManager.AddUser();
+
+            await Clients.All.SendAsync("UpdateNbUsers", _pizzaManager.NbConnectedUsers);
+
             await base.OnConnectedAsync();
         }
 
         public override async Task OnDisconnectedAsync(Exception? exception)
         {
+            _pizzaManager.RemoveUser();
+
+            await Clients.All.SendAsync("UpdateNbUsers", _pizzaManager.NbConnectedUsers);
+
             await base.OnConnectedAsync();
         }
 
         public async Task SelectChoice(PizzaChoice choice)
         {
+
+            string groupName = _pizzaManager.GetGroupName(choice);
+
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+
+            int pizzaPrice= _pizzaManager.PIZZA_PRICES[(int)choice];
+            await Clients.Caller.SendAsync("UpdatePizzaPrice", pizzaPrice);
+            
+            int nbPizza = _pizzaManager.NbPizzas[(int)choice];
+            int money = _pizzaManager.Money[(int)choice];
+            await Clients.Caller.SendAsync("UpdateNbPizzasAndMoney",nbPizza, money);
+
         }
 
         public async Task UnselectChoice(PizzaChoice choice)
         {
+
+            string groupName = _pizzaManager.GetGroupName(choice);
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
+
         }
 
         public async Task AddMoney(PizzaChoice choice)
         {
+
+            _pizzaManager.IncreaseMoney(choice);
+            string groupName = _pizzaManager.GetGroupName(choice);
+            int money = _pizzaManager.Money[(int)choice];
+            await Clients.Group(groupName).SendAsync("UpdateMoney", money);
+
         }
 
         public async Task BuyPizza(PizzaChoice choice)
         {
+
+            _pizzaManager.BuyPizza(choice);
+
+            string groupName= _pizzaManager.GetGroupName(choice);
+            int nbPizza=  _pizzaManager.NbPizzas[(int)choice];
+            int money = _pizzaManager.Money[(int)choice];
+
+            await Clients.Group(groupName).SendAsync("UpdateNbPizzasAndMoney", nbPizza, money);
+
         }
     }
 }
--- question2/ngSignalR/src/app/app.component.ts
+++ question2/ngSignalR/src/app/app.component.ts
@@ -33,20 +33,49 @@ export class AppComponent {
       .build();
 
     // TODO: Mettre isConnected à true seulement une fois que la connection au Hub est faite
-    this.isConnected = true;
+      this.hubConnection
+      .start()
+      .then(() => {
+        this.isConnected = true;
+        console.log("Connected to hub successfully!");
+      })
+      .catch(err => console.log('Error while starting connection: ' + err))
+
+    this.hubConnection.on("UpdateNbUsers", (nbUsers) => {
+      this.nbUsers = nbUsers;
+    });
+
+    this.hubConnection.on("UpdatePizzaPrice", (pizzaPrice) => {
+      this.pizzaPrice = pizzaPrice;
+    });
+
+    this.hubConnection.on("UpdateNbPizzasAndMoney", (nbPizza, money) => {
+      this.nbPizzas = nbPizza;
+      this.money = money;
+    });
+
+    this.hubConnection.on("UpdateMoney", (money) => {
+      this.money = money;
+    });
+
   }
 
   selectChoice(selectedChoice:number) {
     this.selectedChoice = selectedChoice;
+    this.hubConnection?.invoke("SelectChoice", selectedChoice);
   }
 
   unselectChoice() {
     this.selectedChoice = -1;
+    this.hubConnection?.invoke("UnselectChoice", this.selectedChoice);
   }
 
   addMoney() {
+    this.hubConnection?.invoke("AddMoney", this.selectedChoice);
   }
 
   buyPizza() {
+    this.hubConnection?.invoke("BuyPizza", this.selectedChoice);
   }
+  
 }
